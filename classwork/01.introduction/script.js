c0411967ebf6e197aba3c23e6193f8c035e6ade7
unknown <ramilaj@code.edu.az>
2024-05-14 19:15:55 +0400

    01.introduction

---
+++ classwork/01.introduction/script.js
@@ -0,0 +1,148 @@
+// keywords- var, let, const
+// const user = 'Ramil'
+// let age = 30
+// age = 31
+
+// var -keyword
+// user- variable
+// "Ramil" - value
+
+// {const city = 'Baku'
+
+// console.log(user);
+// }
+
+
+
+// console.log(city);
+
+
+// named conversions
+
+// 1.CamelCase
+// 2.PascalCase
+// 3.snake_case
+// 4.kebab-case 
+
+
+// let firstName = "Ramil"
+// console.log(firstName);
+
+// let FirstName = "Ramil"
+// console.log(FirstName);
+
+
+// let first_name = "Ramil"
+// console.log(firstName);
+
+
+// let first-name = "Ramil"
+// console.log(firstName);
+
+
+// let firstName = "Ramil"
+// let lastName = `"Janmirzayev championchampionchampionchampion
+// championchampionchampionchampion`
+// let fullName =`name:${firstName} / surname:${lastName}`
+// console.log(fullName);
+
+
+// let firstName = 'Ramil'
+// let secondName = 'Janmirzayev'
+// let surName = 'Agamirza'
+// let age= 30
+// let fullInfo =`name:${firstName} / lastname:${secondName} / fathername:${surName} number:${age}`
+
+// console.log(fullInfo);
+
+
+
+// let age = 25
+// let age2 = 45
+// console.log(age +age2)
+
+// let age = 25
+// let age2 = 45
+// console.log(age - age2)
+
+// let age = 25
+// let age2 = 45
+// console.log(age / age2)
+
+// let age = 25
+// let age2 = 45
+// console.log(age *age2)
+
+// let age = 25
+// let age2 = 45
+// console.log(age % age2)
+
+
+// let age = 25
+// let age2 = 45
+// console.log(age ^age2)
+
+
+
+// console.log(`1` + `2` + `6`);
+// console.log(1 + 1  + `1` * 5 *3);
+// console.log(1 + 1 +4 + `1`)
+// console.log(`1` + 1 +2);
+
+
+// Nan = not a number
+
+// '=' '+' '-' '/' '*' '%' - math operators
+// + - concat
+
+//  console.log(5 % 4);
+
+// Data types JS - primitive and non-primitive
+
+// primitive
+// string, bigInt, number, boolean, Symbol, null, undefined
+
+// non 
+// Object
+
+// string
+// 'Ramil'
+// 'true'
+// 'string'
+// '9'
+// 'null'
+
+
+// number all numbers - (pos and CSSMathNegate, 0)
+
+// bigInt - 10n
+
+// boolean - true and false 
+
+// let a = undefined
+// console.log(typeof a);
+// to check typeof value - typeof
+
+// conditinal if, else 
+
+// = (assignment)
+// == (only value) !=
+// === (type and value) !==
+
+// || origin
+// && and 
+// !
+
+// console.log(4 === '4');
+// console.log(4 === 4
+// );
+// let age= '17'
+// let user= 'Ramil'
+
+// if (age == 17 || user !== 'Ramil'){
+//     console.log('you are allowed');
+// }  
+// else {
+//     console.log('u are not');
+// }
+
