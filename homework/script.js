bdcc81d4d72886cb402df957d7a5be35b7f87661
Ramilaj <ramilaj@code.edu.az>
2024-05-17 19:42:10 +0400

    Update script.js

--- homework/script.js
+++ homework/script.js
@@ -1,2 +1,75 @@
 // Task 1
 
+// console.log(12 * 8);    96
+
+// let x= 10 / 2
+// console.log(x);         5
+
+// let num1= 20
+// let num2= 17
+// console.log(num1 + num2);         37
+
+// let firstName = "Ramil"
+// let surName ="Janmirzayev"
+// let year = '1994'
+// let NameInfo= firstName + " / " + surName +" / " + year
+
+// console.log(NameInfo)      
+
+
+// console.log(17 % 12);             5
+
+
+// var cityName= 'Baku'
+// console.log(cityName);
+// var cityName = "Ganja"
+// console.log(cityName);
+
+
+// Task 2
+
+// let a = 1
+// let b = 1
+// let c= ++a
+// let d= b++
+
+// console.log(c); 2
+// console.log(d); 1
+// console.log(b); 2
+
+
+// Task 3
+
+// let a = 2
+// let x= 1 + (a *= 2)
+// console.log(x);      5     
+
+
+// Task 4
+
+// console.log('test1',test);            undefined
+// {
+//     let test = 'something'
+//     console.log('test2',test);        something
+// }
+// console.log('test3', test);           undefined
+
+// console.log('test1', test);  // ?     undefined
+// {
+//     var test = "something"
+//     console.log('test2', test);       something 
+// }
+// console.log('test3', test);           something 
+
+
+// Task 5
+
+// let surname = 'Zakirli'                 string
+// let name = 'Ulfat'                      string
+// let year = '2000'                       string
+// year= '2000'                            string
+// let city;                               undefined
+// let qualification = null                object
+// let obj = { name: 'ulfat' }             object
+// let arr = ['a', 'b', 'c']               function
+// console.log(typeof year);
